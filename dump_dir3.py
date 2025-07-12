13b63a47d18215b6c4afb7311c179fa5e41a2a05
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-07-12 10:54:37 -0500

    Cambios menores en la presentación

--- dump_dir3.py
+++ dump_dir3.py
@@ -1,25 +1,165 @@
+#!/usr/bin/env python3
+
+import sys
+import dataclasses
 import csv
 
-
-def clean_text(s: str):
-    s = s.strip()
-    if s[0] == s[-1] == '"':
-        return s[1:-1]
-    return s
+from filters import clean_text
+from filters import clean_integer
+from filters import slugify
 
 
-def main():
+@dataclasses.dataclass
+class Organismo:
+    """Estructura de datos para DIR3.
+    """
+    id_dir3: str
+    id_sirhus: int
+    nombre_organismo: str
+    nivel: int
+    tipo: str
+    depende_de: str
+    es_superior: bool
+    childs: set = dataclasses.field(default_factory=set)
+
+    def __eq__(self, other):
+        return self.id_dir3 == other.id_dir3
+
+    def __lt__(self, other):
+        return self.nombre_organismo < other.nombre_organismo
+
+    def __le__(self, other):
+        return self.nombre_organismo <= other.nombre_organismo
+
+    def __gt__(self, other):
+        return self.nombre_organismo > other.nombre_organismo
+
+    def __ge__(self, other):
+        return self.nombre_organismo >= other.nombre_organismo
+
+    def __str__(self):
+        return (
+            f'{self.es_superior} {self.id_dir3} {self.nombre_organismo}'
+            f' {self.nivel} {self.tipo} <-- {self.depende_de} [{len(self.childs)}]'
+            )
+
+    def __hash__(self):
+        return hash(self.id_dir3)
+
+    def add(self, item):
+        self.childs.add(item)
+    
+    def get_filename(self, ext='csv'):
+        return f'{self.id_dir3}-{slugify(self.nombre_organismo)}.{ext}'
+
+
+
+def iter_organigrama():
+    """Iterador sobre el organigrama.
+    """
     with open('organigrama.csv', 'r', encoding='utf-8') as source:
         reader = csv.reader(source, delimiter=';', quotechar='"')
         next(reader) # Ignorar primera fila de nombres
         for row in reader:
-            
-            id_dir3 = row[1]
-            id_sirhus = row[2]
-            nombre_organismo = clean_text(row[3])
-            depende_de = row[7]
-            print(id_dir3, nombre_organismo, '<--', depende_de)
+            yield Organismo(
+                id_dir3=row[1],
+                id_sirhus=clean_integer(row[2]),
+                nombre_organismo=clean_text(row[3]),
+                nivel=clean_integer(row[4]),
+                tipo=row[5],
+                depende_de=row[7],
+                es_superior=(row[7] == 'A05003638'),
+                )
+
+def create_map() -> dict:
+    """Devuelve el mapa del organigrama.
+    """
+    orgs = list(iter_organigrama())
+    orgs_map = { org.id_dir3: org for org in orgs }
+    for org in orgs:
+        if org.depende_de in orgs_map:
+            orgs_map[org.depende_de].add(org)
+    return orgs_map
+
+def create_tree(orgs_map) -> set:
+    return {org for org in orgs_map.values() if org.es_superior }
+
+
+def list_organismo(id_dir3, level=0, tree=None):
+    """Lista los organismos dependedientes de otro.
+    """
+    if tree is None:
+        tree = create_map()
+    org = tree[id_dir3]
+    print(
+        '   ' * level,
+        '└' if level > 0 else ' ',
+        org.nombre_organismo,
+        org.id_dir3,
+        )
+    for child in sorted(org.childs):
+        list_organismo(child.id_dir3, level+1, tree=tree)
+
+
+
+def list_top():
+    """Lista todos los organismos de nivel superior.
+    """
+    tree = create_tree(create_map())
+    for org in tree:
+        print(f'Creando {org.get_filename()}')
+
+
+def travel(org):
+    yield org
+    if org.childs:
+        for child in org.childs:
+            yield from travel(child)
+
+
+def make_graph(id_dir3=None):
+    if id_dir3 is None:
+        with open('organigrama.dot', 'w') as f_out:
+            f_out.write('digraph organigrama {\n')
+            f_out.write('    rankdir=LR\n')
+            for org in iter_organigrama():
+                f_out.write(
+                    f'    {org.id_dir3}'
+                    f' [label="{org.nombre_organismo}"]\n'
+                    )
+                f_out.write(f'    {org.depende_de} -> {org.id_dir3}')
+            f_out.write('}')
+    else:
+        orgs = create_map()
+        org = orgs[id_dir3]
+        filename = org.get_filename('dot')
+        print(f'Creado {filename}', end=' ')
+        with open(filename, 'w', encoding='utf-8') as f_out:
+            f_out.write('digraph {\n')
+            f_out.write('    rankdir=LR\n')
+            for node in travel(org):
+                print('.', end='')
+                f_out.write(
+                    f'    {node.id_dir3}'
+                    f' [label="{node.nombre_organismo}"]\n'
+                    )
+                f_out.write(f'    {node.depende_de} -> {node.id_dir3}\n')
+            f_out.write('}')
+        print('[Ok]')
+
+
+
+
+
 
 
 if __name__ == "__main__":
-    main()
+    argc = len(sys.argv)
+    if argc == 1:
+        list_top()
+        make_graph()
+    if argc == 2:
+        id_dir3 = sys.argv[1]
+        print(f'Analizando {id_dir3}')
+        list_organismo(id_dir3)
+        make_graph(id_dir3)
