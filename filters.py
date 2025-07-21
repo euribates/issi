fdf357668a1c39ac4de594ba9200f3154f16af75
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2025-07-21 12:09:49 +0100

    Mejoras y tests de modulo filters

--- filters.py
+++ filters.py
@@ -2,42 +2,135 @@
 
 import re
 
+from typing import Optional
 
-_TABLA_SLUG = ''.maketrans({
-    'á': 'a',
-    'é': 'e',
-    'í': 'i',
-    'ó': 'o',
-    'ú': 'u',
-    'ü': 'u',
-    'ñ': 'nn',
-    'Á': 'a',
-    'É': 'e',
-    'Í': 'i',
-    'Ó': 'o',
-    'Ú': 'u',
-    'Ü': 'u',
-    'Ñ': 'nn',
-    })
+def clean_text(text: str) -> str:
+    """Limpia el formato de texto.
 
+    - Si hay espacios al principio o al final se eliminan
+    - Si tiene comillas dobles al principio y al final las elimina.
+    - Si tiene comillas simples al principio y al final las elimina.
 
+    >>> assert clean_text('"hola"') == 'hola'
 
-def slugify(text: str) -> str:
-    text = text.lower().strip()
-    text = re.sub(r'[^\w\s-]', '', text)
-    text = re.sub(r'[\s_-]+', '-', text)
-    text = re.sub(r'^-+|-+$', '', text)
-    text = text.translate(_TABLA_SLUG)
-    return text
+    Params:
+        
+        - text (str): La cadena de texto a limpiar.
 
+    Returns:
 
-def clean_text(text:str) -> str:
+        Una cadena de texto limpia.
+    """
+    text = text.strip()
     if text[0] == text[-1] == '"':
+        return text[1:-1]
+    if text[0] == text[-1] == "'":
         return text[1:-1]
     return text
 
 
-def clean_integer(text: str) -> int|None:
-    if text is None or text == '' or text == '_U':
+def clean_integer(text: str) -> Optional[int]:
+    """Interpreta una cadena de texto como un número entero.
+
+    Los valores especiales '' (cadena vacia) o `'_U'` se 
+    interpretan como `None`.
+
+    >>> assert clean_integer('123') == 123
+    >>> assert clean_integer('_U') is None
+
+    Params:
+
+        text (str): Una cadena de texto que contiene un número
+                    entero, como `'123'`.
+
+    Returns:
+
+        Un entero, o `None`.
+    """
+    if text in {'_U', '', None}:
         return None
     return int(text)
+
+
+_SLUGIFY_MAP = {
+    32: 45,  # space -> hyphen
+    33: None,  # exclamation mark
+    34: None,  # double quotes
+    35: None,  # hash
+    36: None,  # dollar
+    37: None,  # percent
+    38: None,  # ampersand
+    39: None,  # simple quote
+    40: None,  # open par
+    41: None,  # close par
+    42: None,  # asterisk
+    43: 45,  # plus -> hyphen
+    44: None,  # comma
+    46: None,  # dot or full stop
+    47: 45,  # slash -> hyphen
+    58: 45,  # colon -> hyphen
+    59: 45,  # semicolon -> hyphen
+    60: None,  # open angled bracket
+    61: 45,  # equals -> hyphen
+    62: None,  # close angled bracket
+    63: None,  # question mark
+    64: 45,  # @ -> hyphen
+    91: None,  # open bracket
+    92: 45,  # backslash -> hyphen
+    93: None,  # close bracket
+    94: 45,  # caret -> hyphen
+    95: 45,  # underscore -> hyphen
+    96: None,  # grave accent
+    123: None,  # open brace
+    124: 45,  # pipe -> hyphen
+    125: None,  # close brace
+    126: 45,  # equivalency sign (~) -> hyphen
+    133: 45,  # ellipsis
+    191: None,  # open question mark
+    193: 65,
+    201: 69,
+    205: 73,
+    209: 78,
+    211: 79,
+    218: 85,
+    220: 85,
+    225: 97,  # a
+    233: 101,
+    237: 105,
+    241: 110,
+    243: 111,
+    250: 117,
+    252: 117,
+    8230: 45,  # ellipsis
+}
+
+_SLUGIFY_PAT_MULTIPLE_HYPHENS = re.compile(r'--+')
+
+
+def slugify(texto: str) -> str:
+    """Transforma texto a un valor válido para usarse como _slug_
+
+    Sustituye espacion por el caracter `'-'`, elimina caracteres
+    especiales, convierte vocales acentuadas, reduce repeticiones,
+    convierte mayúsculas a minúsculas y otras modificaciones que
+    permiten usar el resultado como un valor seguro para ser usado como
+    nombre de fichero, parte de la URL, etc.
+
+    >>> slugify('Hola, mundo') == 'hola-mundo'
+
+    Params:
+
+        text (str): El texto a transformar
+
+    Returns:
+
+        Una cadena de texto apta para ser usada como nombre de un fichero,
+        parte de una URL, etc. 
+    """
+    result = texto.lower()
+    result = result.replace('ñ', 'nn')
+    result = result.replace('€', '-euros')
+    result = result.translate(_SLUGIFY_MAP)
+    result = ''.join([_ for _ in result if ord(_) < 129])
+    result = _SLUGIFY_PAT_MULTIPLE_HYPHENS.sub('-', result)
+    return result
--- test_filters.py
+++ test_filters.py
@@ -4,8 +4,145 @@ import pytest
 
 import filters
 
+# Tests for slugify
+
 def test_slugify_simple():
     assert filters.slugify("Hola, mundo árbol") == "hola-mundo-arbol"
+
+def test_slugify_hola():
+    assert filters.slugify('holá') == 'hola'
+
+def test_slugify_slug():
+    assert filters.slugify('hola123') == 'hola123'
+
+def test_slugify_arbol():
+    assert filters.slugify('árbol') == 'arbol'
+
+
+def test_slugify_vocals():
+    assert filters.slugify('áéíóúü-ÁÉÍÓÚÜ') == 'aeiouu-aeiouu'
+
+
+def test_slugify_enies():
+    assert filters.slugify('año') == 'anno'
+    assert filters.slugify('AÑO') == 'anno'
+
+
+def test_slugify_parentesis():
+    assert filters.slugify('(hola)') == 'hola'
+
+
+def test_slugify_corchetes():
+    assert filters.slugify('[hola]') == 'hola'
+
+
+def test_slugify_llaves():
+    assert filters.slugify('[hola]') == 'hola'
+
+
+def test_slugify_pipe():
+    assert filters.slugify('hola|mundo') == 'hola-mundo'
+
+
+def test_slugify_space():
+    assert filters.slugify('hola mundo') == 'hola-mundo'
+
+
+def test_slugify_symbols():
+    assert filters.slugify('!"#$%&\'*,.') == ''
+
+
+def test_slugify_plus():
+    assert filters.slugify('hola+mundo') == 'hola-mundo'
+
+
+def test_slugify_hyphen():
+    assert filters.slugify('hola-mundo') == 'hola-mundo'
+
+
+def test_slugify_slash():
+    assert filters.slugify('hola/mundo') == 'hola-mundo'
+
+
+def test_slugify_backslash():
+    assert filters.slugify('hola\\mundo') == 'hola-mundo'
+
+
+def test_slugify_numbers():
+    assert filters.slugify('0123456789') == '0123456789'
+
+
+def test_slugify_lowercase():
+    expected = 'abcdefghijklmnnnopqrstuvwxyz'
+    assert filters.slugify('abcdefghijklmnñopqrstuvwxyz') == expected
+
+
+def test_slugify_uppercase():
+    expected = 'abcdefghijklmnnnopqrstuvwxyz'
+    assert filters.slugify('ABCDEFGHIJKLMNÑOPQRSTUVWXYZ') == expected
+
+
+def test_slugify_colon():
+    assert filters.slugify('hola:mundo') == 'hola-mundo'
+
+
+def test_slugify_semicolon():
+    assert filters.slugify('hola;mundo') == 'hola-mundo'
+
+
+def test_slugify_angled_brackets():
+    assert filters.slugify('<hola mundo>') == 'hola-mundo'
+
+
+def test_slugify_equals():
+    assert filters.slugify('hola=mundo') == 'hola-mundo'
+
+
+def test_slugify_question_marks():
+    assert filters.slugify('¿hola?') == 'hola'
+
+
+def test_slugify_exclamation_marks():
+    assert filters.slugify('¡hola!') == 'hola'
+
+
+def test_slugify_at():
+    assert filters.slugify('hola@mundo') == 'hola-mundo'
+
+
+def test_slugify_caret():
+    assert filters.slugify('hola^mundo') == 'hola-mundo'
+
+
+def test_slugify_underscore():
+    assert filters.slugify('hola_mundo') == 'hola-mundo'
+
+
+def test_slugify_grave_accent():
+    assert filters.slugify('hola`mundo') == 'holamundo'
+
+
+def test_slugify_equivalency_sign():
+    assert filters.slugify('hola~mundo') == 'hola-mundo'
+
+
+def test_slugify_multiple_hyphens():
+    assert filters.slugify('hola-~mundo') == 'hola-mundo'
+    assert filters.slugify('hola-------mundo') == 'hola-mundo'
+    assert filters.slugify('hola-~-=-_-^-@-mundo') == 'hola-mundo'
+
+
+def test_slugify_euro():
+    assert filters.slugify('23€') == '23-euros'
+
+
+def test_slugify_ellipsis():
+    assert filters.slugify('hola…mundo') == 'hola-mundo'
+
+
+def test_slugify_ejemplo():
+    expected = 'charla-hablamos-bien-los-canarios'
+    assert filters.slugify('Charla: ¿Hablamos bien los canarios?') == expected
 
 
 if __name__ == "__main__":
