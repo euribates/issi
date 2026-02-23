74f1786deb02a5043eaca3d923b38442bcce4057
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2026-02-23 14:13:59 +0000

    Reset de migraciones de sistemas, directorio y juriscan

--- export_database.sh
+++ export_database.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 python -Xutf8 ./manage.py dumpdata glosario --indent 4 > glosario/fixtures/glosario.json
-python -Xutf8 ./manage.py dumpdata sistemas --indent 4 > sistemas/fixtures/sistemas.json
 python -Xutf8 ./manage.py dumpdata normativa --indent 4 > normativa/fixtures/normativa.json
+
+python -Xutf8 ./manage.py dumpdata sistemas --indent 4 > sistemas/fixtures/sistemas.json
 python -Xutf8 ./manage.py dumpdata directorio --indent 4 > directorio/fixtures/directorio.json
 python -Xutf8 ./manage.py dumpdata juriscan --indent 4 > juriscan/fixtures/juriscan.json
 zip  -r uploads.zip uploads/
