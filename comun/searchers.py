51db358d20ca98d1fd633d6a439f5f552d90e4fe
Juan Ignacio Rodríguez de León <euribates@gmail.com>
2026-05-05 13:12:42 +0100

    Módulo para crear una fachada común para las consultas

---
+++ comun/searchers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from django.db.models import Q
+
+from sistemas.models import Sistema
+from directorio.models import Empresa
+from sistemas.parsers import parse_uuid
+
+    
+
+def search_empresas(cls, query):
+    qs = Empresa.objects.all()
+    if query:
+        qs = qs.filter(
+            Q(nombre_empresa__icontains=query) |
+            Q(nif__icontains=query)
+            )
+    return qs
+
+
+def search_sistemas(query):
+    qs = Sistema.objects.select_related('organismo').all()
+    if query:
+        qs = qs.filter(
+            Q(nombre_sistema__icontains=query) |
+            Q(codigo__icontains=query) |
+            Q(finalidad__icontains=query) |
+            Q(descripcion__icontains=query) |
+            Q(organismo__nombre_organismo__icontains=query) |
+            Q(organismo__dir3__istartswith=query) 
+            )
+        is_uuid = parse_uuid(query)
+        if is_uuid.is_success():
+            qs = qs | Sistema.objects.filter(uuid_sistema=is_uuid.value)
+    return qs
